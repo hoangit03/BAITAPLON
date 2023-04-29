23329bfdd536340bcae924a5014bd8b157998b95
DESKTOP-AFDU1PD\CCLaptop <hoangit2003xoan@gmail.com>
2023-04-30 00:11:50 +0700

    Login js hoan thanh

--- js/js.js
+++ js/js.js
@@ -1,20 +1,18 @@
 
 
+var accountAPI = 'http://localhost:3000/account';
 
-var accuonts = [
-  {
-    id : 1,
-    email: 'thuong@gmail.com',
-    password: '12345678'
-  },
-  {
-    id: 2,
-    email: 'hoang@gmail.com',
-    password: '12345678'
-  }
-]
+// Function getAccount
+function getAccount(callback){
+  fetch(accountAPI)
+    .then(function(response){
+      return response.json();
+    })
+    .then(callback)
+}
 
-function checkLogin(form){
+
+function checkLogin(form,accuonts){
   let btn_submit =  form.querySelector('button[type="submit"]')
   let inputEmail = form.querySelector('input[type="email"]')
   let inputPass = form.querySelector('input[type="password"]')
@@ -22,20 +20,22 @@ function checkLogin(form){
     let error = this.parentElement.querySelector('.error-messages')
     error.innerHTML = ""
   }
-  accuonts.forEach(accuont=>{
-    btn_submit.onclick = function(){
-      event.preventDefault();
-      if(accuont.email == inputEmail.value && accuont.password == inputPass.value){
-
+  
+  btn_submit.addEventListener('click',function(){
+    event.preventDefault();
+    email = inputEmail.value
+    pass = inputPass.value
+    accuonts.forEach(accuont=>{
+      if(accuont.email == email && accuont.password == pass){
         window.location = './index.html'
       }
       else{
         let error = inputEmail.parentElement.querySelector('.error-messages')
-        error.innerHTML = 'Tài khoản không hợp lệ. Vui lòng đăng nhập lại!'
-        inputEmail.value = ""
+        error.innerHTML = 'Tài khoản hoặc mật khẩu không chính xác. Vui lòng đăng nhập lại!'
         inputPass.value = ""
       }
-    }
+    })
+    
   })
 }
 
@@ -58,12 +58,13 @@ function nextPage(forms){
 
 }
 
-function checkRegister(form){
+function checkRegister(form,accuonts){
   let btn_submit =  form.querySelector('button[type="submit"]')
   let inputEmail = form.querySelector('input[type="email"]')
   let inputPass = form.querySelector('input[type="password"]')
   let inputPassConfi = form.querySelector('input[id="confirmPassword"]')
   let inputs = form.querySelectorAll('input')
+  let errors = form.querySelectorAll('.error-messages')
   inputs.forEach(input=>{
     input.onfocus = function(){
       let error = this.parentElement.querySelector('.error-messages')
@@ -83,63 +84,83 @@ function checkRegister(form){
     }
   }
   
-  for (let index = 0; index < accuonts.length; index++) {
-    
   
-    btn_submit.onclick = function(){
-      event.preventDefault();
-      inputs.forEach(input=>{
-        if(input.value == ''){
-          let error = input.parentElement.querySelector('.error-messages')
-          error.innerHTML = "Trường này không được bỏ trống"
-        }
-        else{
-          var a = [{
-            id: accuonts.length,
-            email: inputEmail.value,
-            password: inputPass.value
-          }]
-          accuonts.push(a)
-          
-          location.reload()
-        }
-      })
-      if(accuonts[index].email == inputEmail.value){
+  btn_submit.addEventListener('click',function(){
+    event.preventDefault();
+    let dk = 0;
+    accuonts.forEach(accuont=> {
+      if(accuont.email == inputEmail.value){
         let error = inputEmail.parentElement.querySelector('.error-messages')
         error.innerHTML = "Email này đã đăng ký tài khoản!"
       }
-      
-      
+      else{
+        if(checkEmail(inputEmail.value)==false){
+          let error = inputEmail.parentElement.querySelector('.error-messages')
+          error.innerHTML = "Email không hợp lệ!"
+        }
+        
+      }
+    })
+    
+    inputs.forEach(input=>{
+      if(input.value == ''){
+        let error = input.parentElement.querySelector('.error-messages')
+        error.innerHTML = "Trường này không được bỏ trống"
+      }
+    })
+    errors.forEach(error=>{
+    if(!error.textContent)
+      dk++;
+    })
+    if(dk == errors.length){
+      var data = {
+        email: inputEmail.value,
+        password: inputPass.value
+      }
+      createAccount(data);
     }
-  }
+    
+  })
+  
+
+}
+
+function createAccount(data){
+  var option = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+  fetch(accountAPI,option)
+    .then(function(data){
+      location.reload();
+    })
+
+}
+
+function checkEmail(value){
+  let regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+  return regex.test(value)
 }
 
 
 function LoginPage(){
   var forms = document.querySelectorAll('#content_login form')
-  // Chức năng singup
+  getAccount(function(accuonts){
+// Chức năng singup
 // --------------------------------------------------------------------
-  checkRegister(forms[1])
-
+    checkLogin(forms[0],accuonts)
 // Chức năng login
 // ------------------------------------------------------------------------
-  checkLogin(forms[0])
+    checkRegister(forms[1],accuonts)
 
+  })
+  
 // Login -> singup
 // ---------------------------------------------------------------------------
   nextPage(forms);
-
-
-
-
-// login
-// ------------------------------------------
-// forms.forEach(form=>{
-//   console.log(form);
-// })
-
 }
-
-
 LoginPage();
 // --------------------------------------------------------------------
